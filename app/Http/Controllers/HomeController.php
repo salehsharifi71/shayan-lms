4359ee85465690efcfbc879db111030ffec2d0ff
Mrfenj <bia2znd@gmail.com>
2020-09-03 13:01:35 +0430

    تغییر نام

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,7 +90,9 @@ class HomeController extends Controller
         if(strlen($name)<2){
             $name='مهمان';
         }
+        $name= 'salam';
         $bbb=new BBBController();
+//        return '<iframe=src="'.$bbb->joinRoomAdmin($meet,$name).'" width="100%" height="100%">';
         return redirect()->intended($bbb->joinRoomAdmin($meet,$name));
     }
     public function demo(){

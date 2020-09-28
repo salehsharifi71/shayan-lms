00ce4f35b6edbf8bbd7f15205207632adbdffb1a
Mrfenj <bia2znd@gmail.com>
2020-09-28 16:57:28 +0330

    دیتابیس بلاگ

---
+++ database/migrations/2020_09_28_130114_create_blogs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('organizer_id')->default(0);
+            $table->string('title');
+            $table->string('thumb')->nullable();
+            $table->text('description');
+            $table->text('meta_description')->nullable();
+            $table->string('slug');
+            $table->boolean('isDeleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}

dc067c72fbf7c4e8598ea936369ca5f43f7392ce
saeh sharifi <bia2znd@gmail.com>
2022-03-23 12:38:07 +0430

    بالا اوردن راستین

--- resources/views/iframePrivate.blade.php
+++ resources/views/iframePrivate.blade.php
@@ -2,32 +2,24 @@
 @section('title')  @endsection
 @section('headMeta') <style>.footer-area{display:none}</style> @endsection
 @section('siteContent')
-
-    <!-- Start Page Title Area -->
-    <section class="page-title-banner">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2>
-
-                        @lang('info.classNotFound')
-                    </h2>
-                </div>
+    <!--begin::Forms Widget 2-->
+    <div class="card card-custom gutter-b">
+        <div class="card-header">
+            <div class="card-title">
+                <h3 class="card-label">خطا در ارتباط</h3>
             </div>
         </div>
+        <!--begin::Body-->
+        <div class="card-body">
 
-        <div class="shape1"><img src="/assets/publicTheme/img/shape1.png" alt="img"></div>
-        <div class="shape2"><img src="/assets/publicTheme/img/shape2.png" alt="img"></div>
-        <div class="shape3"><img src="/assets/publicTheme/img/shape3.png" alt="img"></div>
-        <div class="shape6"><img src="/assets/publicTheme/img/shape6.png" alt="img"></div>
-        <div class="shape8 rotateme"><img src="/assets/publicTheme/img/shape8.svg" alt="shape"></div>
-        <div class="shape9"><img src="/assets/publicTheme/img/shape9.svg" alt="shape"></div>
-
-    </section>
-    <!-- End Page Title Area -->
+            @lang('info.classNotFound')
+        </div>
+        <!--end::Body-->
+    </div>
+    <!--end::Forms Widget 2-->
 
     <!-- Start Contact Info Area -->
-    <iframe id="showClass" allow="microphone *; camera *; display-capture *" allowfullscreen="" data-hj-allow-iframe="" mozallowfullscreen="" msallowfullscreen="" oallowfullscreen="" src="{{$url}}" webkitallowfullscreen=""></iframe>
+    <iframe style="width: 100%;height: 100%;top: 0;right: 0;position: fixed;z-index: 100000000000000000;" id="showClass" allow="microphone *; camera *; display-capture *" allowfullscreen="" data-hj-allow-iframe="" mozallowfullscreen="" msallowfullscreen="" oallowfullscreen="" src="{{$url}}" webkitallowfullscreen=""></iframe>
 
     <!-- End Contact Info Area -->
 @endsection
